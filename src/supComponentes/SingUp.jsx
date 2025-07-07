ee87207503e52a936146ffd60853ab05cdcb7ac9
Alexander/ <100982867+AjessDEV@users.noreply.github.com>
2025-07-07 11:15:57 -0500

    Update SingUp.jsx

--- src/supComponentes/SingUp.jsx
+++ src/supComponentes/SingUp.jsx
@@ -188,7 +188,7 @@ export default function SingUp() {
             <input type="password" placeholder="Contraseña" className="p-[10px_8px] border-2 border-[#00000030] rounded-[10px] outline-none mb-3 w-full text-[18px]" value={password} onChange={(e) => setPassword(e.target.value)}/>
             <input type="password" placeholder="Confirmar contraseña" className="p-[10px_8px] border-2 border-[#00000030] rounded-[10px] outline-none mb-3 w-full text-[18px]" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
             <br />
-            <button onClick={handleRegister} className={`font-bold w-full py-4 ${!username || !email || email !== "" && dominiosPermitidos.some((dominio) => email.includes(dominio)) || !Sucursal|| !rol || !password || !confirmPassword ? 'bg-[#e0e0e0] text-[#707070] pointer-events-none' : 'bg-[#ffa600] text-white'} text-[20px] rounded-[10px] transition-all ease-in-out duration-200`}>Registrar</button>
+            <button onClick={handleRegister} className={`font-bold w-full py-4 ${!username || !email || email !== "" && dominiosPermitidos.some((dominio) => email.includes(dominio)) || Sucursal !== 'Seleccionar Sucursal' || rol !== 'Seleccionar Rol' || !password || !confirmPassword ? 'bg-[#e0e0e0] text-[#707070] pointer-events-none' : 'bg-[#ffa600] text-white'} text-[20px] rounded-[10px] transition-all ease-in-out duration-200`}>Registrar</button>
             
       </div>
     </>
